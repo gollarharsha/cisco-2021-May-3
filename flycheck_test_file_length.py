baf210c6fa6939fa79f683ed7eb7897f60f0ccf9
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 19:14:58 +0300

    gitautosync update 185

--- flycheck_test_file_length.py
+++
@@ -1,9 +0,0 @@
-from file_length import file_length
-
-
-def test_good_file_length():
-    assert file_length('/etc/passwd') == 7579
-
-
-def test_bad_file_length():
-    assert file_length('/etcccc/passwddddd') == 7579
