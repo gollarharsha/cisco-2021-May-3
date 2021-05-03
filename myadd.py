a817e042fec37441ba306ba699cd8518ae784bd4
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 16:47:21 +0300

    gitautosync update 38

--- myadd.py
+++ myadd.py
@@ -2,5 +2,13 @@ def add(first, second):
     return first + second
 
 
-def test_add_ints():
+def test_add_positive_ints():
     assert add(2, 2) == 4
+
+
+def test_add_negative_ints():
+    assert add(-2, -2) == -4
+
+
+def test_add_mixed_ints():
+    assert add(2, -2) == 0
