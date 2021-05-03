1ec940df5ed0334dd9b0e65a80e3ba485df4540c
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 19:51:29 +0300

    gitautosync update 248

--- firstlast.py
+++ firstlast.py
@@ -2,14 +2,4 @@ def firstlast(seq):
     if not seq:
         return seq
 
-    if isinstance(seq, str):
-        return seq[0] + seq[-1]
-
-    elif isinstance(seq, list):
-        return [seq[0], seq[-1]]
-
-    elif isinstance(seq, tuple):
-        return (seq[0], seq[-1])
-
-    else:
-        raise TypeError('Not a sequence!')
+    return seq[:1] + seq[-1:]
