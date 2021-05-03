81f9efed3bbd88bbc0e8d74fecd97d1f1128cd06
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 23:17:51 +0300

    gitautosync update 64

--- flycheck_test_greeting.py
+++
@@ -1,7 +0,0 @@
-from greeting import hello
-
-
-def test_hello(capsys):
-    hello()
-    captured_out, captured_err = capsys.readouterr()
-    assert captured_out == 'Hello out there!'
--- test_greeting.py
+++ test_greeting.py
@@ -1,6 +1,7 @@
 from greeting import hello
 
 
-def test_hello():
-    output = hello()
-    assert output == 'Hello out there!'
+def test_hello(capsys):
+    hello()
+    captured_out, captured_err = capsys.readouterr()
+    assert captured_out == 'Hello out there!'
