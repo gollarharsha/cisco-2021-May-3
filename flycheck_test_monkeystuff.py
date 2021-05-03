37ca5cf6a39a4e67665fc726f55d870ca38d2810
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 22:58:24 +0300

    gitautosync update 26

--- flycheck_test_monkeystuff.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def test_something(monkeypatch):
-    # replace the os.path.join function with a lambda!
-    monkeypatch.setattr('os.path.join', lambda x: '/etc/passwd')
-    print(os.path.join('stuff'))
