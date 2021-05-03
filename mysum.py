582bc5fcb80c7a3a25ee9da261db62e8dab24dde
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 16:31:16 +0300

    gitautosync update 23

--- mysum.py
+++ mysum.py
@@ -7,6 +7,9 @@ def mysum(numbers):
     return total
 
 
-def test_mysum():
+def test_mysum_int():
     assert mysum([10, 20, 30]) == 60
+
+
+def test_mysum_float_and_ints():
     assert mysum([10.2, 20, 30]) == 60.2
