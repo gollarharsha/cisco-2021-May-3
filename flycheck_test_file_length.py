be9afa7e54f6a80975f3018bdafbfe592ecc83dc
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 23:20:56 +0300

    gitautosync update 73

--- flycheck_test_file_length.py
+++ flycheck_test_file_length.py
@@ -2,7 +2,8 @@ import pytest
 from file_length import file_length
 
 def short_file(tmp_path):
-    open(tmp_path
+    with open(tmp_path / 'short_file.txt', 'w') as f:
+        f.write('abcd\n
 
 
 
