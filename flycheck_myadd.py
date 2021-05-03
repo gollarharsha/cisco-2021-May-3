de5b2616262d8a7c9a7db683e52eee5843e1cea9
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 16:46:55 +0300

    gitautosync update 35

--- flycheck_myadd.py
+++ flycheck_myadd.py
@@ -2,5 +2,8 @@ def add(first, second):
     return first + second
 
 
-def test_add_ints():
+def test_add_positive_ints():
+    assert add(2, 2) == 4
+
+def test_add_neints():
     assert add(2, 2) == 4
