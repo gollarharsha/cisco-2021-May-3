6ea2db69435663fda2658304d98ab9ab945583b2
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 18:50:58 +0300

    gitautosync update 153

---
+++ flycheck_test_mysum.py
@@ -0,0 +1,15 @@
+from mysum import mysum
+
+
+def test_mysum_int():
+    assert mysum([10, 20, 30]) == 60
+
+
+def test_mysum_float_and_ints():
+    assert mysum([10.2, 20, 30]) == 60.2
+
+
+def test_mysum_floats():
+    assert isinstance(mysum([1.2, 3.4]), float)
+
+    
