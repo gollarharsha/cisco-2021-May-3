6d60f9fe4503785afd2efd3c03bf9baa8e3491ad
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 17:26:45 +0300

    gitautosync update 83

--- flycheck_test_evens_and_odds.py
+++ flycheck_test_evens_and_odds.py
@@ -2,7 +2,8 @@ from evens_and_odds import evens_and_odds
 
 
 def test_returns_a_dict():
-    output = evens_and
+    output = evens_and_odds([10, 11, 12])
+    assert isinstance(output, dict)
 
 def test_no_numbers():
     output = evens_and_odds([])
