654b2b1778ea8aae66efd31844f910e37c5b7b13
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 21:49:33 +0300

    gitautosync update 310

--- flycheck_test_phone.py
+++ flycheck_test_phone.py
@@ -5,6 +5,7 @@ def test_empty_phone():
     p = Phone()
     assert p.call_history == []
 
+
 def test_make_some_calls():
     p = Phone()
     p.call('12345')
@@ -12,7 +13,8 @@ def test_make_some_calls():
     assert '12345' in p.call_history
     assert '67890' in p.call_history
 
+
 def test_bad_phone_number():
     p = Phone()
     with pytest.raises(ValueError):
-        p.call('badnum1
+        p.call('badnum1')
