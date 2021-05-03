6d9e21c5f56c5b96ff40e93d85fd696450dae96d
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 23:20:28 +0300

    gitautosync update 71

---
+++ flycheck_test_file_length.py
@@ -0,0 +1,15 @@
+import pytest
+from file_length import file_length
+
+def short_file(tmp_path):
+    d = 
+
+
+
+def test_good_file_length():
+    assert file_length('/etc/passwd') == 7579
+
+
+def test_bad_file_length():
+    with pytest.raises(Exception):
+        file_length('/etcccc/adfsa')
