4f47b90196a59fb7fb61f6eb0fd1158e274c14b8
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 19:24:04 +0300

    gitautosync update 210

---
+++ flycheck_test_mysum.py
@@ -0,0 +1,31 @@
+from mysum import mysum
+import pytest
+
+
+# def test_mysum_int():
+#     assert mysum([10, 20, 30]) == 60
+
+
+# def test_mysum_float_and_ints():
+#     assert mysum([10.2, 20, 30]) == 60.2
+
+
+# def test_mysum_floats():
+#     assert isinstance(mysum([1.2, 3.4]), float)
+
+
+@pytest.mark.parameterize('numbers, result',    # what parameter values are we passing?
+                          [
+([10, 20, 30], 60),  # list of tuples, each tuple with param values
+                           ([10.2, 20, 30], 60.2),
+                           ([1.2, 3.4], 4.6)]
+)
+def test_mysum(numbers, result):
+    assert mysum(numbers) == result
+
+
+def test_mysum_strings():
+    with pytest.raises(TypeError):
+        # secret __enter__ method
+        assert mysum(['a', 'b', 'c'])
+        # secret __exit__ method
