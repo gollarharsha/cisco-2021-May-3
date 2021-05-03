c9fd66a689bdfd7e8772bdfa94d3d5360f835a23
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 22:27:57 +0300

    gitautosync update 369

--- test_phone.py
+++ test_phone.py
@@ -7,6 +7,11 @@ def sample_phone():
     return Phone(1)
 
 
+@pytest.fixture
+def good_phone_numbers():
+    return ['12345', '67890', '2468', '1357']
+
+
 def test_empty_phone(sample_phone):
     assert sample_phone.call_history == []
 
@@ -15,11 +20,11 @@ def test_id_number(sample_phone):
     assert sample_phone.id_number == 1
 
 
-def test_make_some_calls(sample_phone):
-    sample_phone.call('12345')
-    sample_phone.call('67890')
-    assert '12345' in sample_phone.call_history
-    assert '67890' in sample_phone.call_history
+def test_make_some_calls(sample_phone, good_phone_numbers):
+    for one_phone_number in good_phone_numbers:
+        assert sample_phone.call(one_phone_number) == 'OK'
+
+    assert sample_phone.call_history == good_phone_numbers
 
 
 def test_bad_phone_number(sample_phone):
