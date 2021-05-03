74d4aa79bde972a82372a6537a747098df5589e9
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 19:48:37 +0300

    gitautosync update 235

--- test_firstlast.py
+++ test_firstlast.py
@@ -3,7 +3,10 @@ import pytest
 
 
 @pytest.mark.parametrize('seq,result',
-                         [('abcde', 'ae'),
+                         [('', ''),
+                          ([], []),
+                          ((), ()),
+                          ('abcde', 'ae'),
                           ([10, 20, 30, 40, 50, 60], [10, 60]),
                           ((100, 200), (100, 200))])
 def test_firstlast(seq, result):
