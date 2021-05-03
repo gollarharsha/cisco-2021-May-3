1b78023fa916fcdb5383b78ac80e442840e181e8
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 21:12:46 +0300

    gitautosync update 255

---
+++ flycheck_person.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Person:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def fullname(self
+
+    def greet(self):
+        return f'Hello, '
