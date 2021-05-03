260f2eb4a45d4379691cb10cad46e9e1821d4aa7
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 23:01:52 +0300

    gitautosync update 35

---
+++ flycheck_test_monkeystuff.py
@@ -0,0 +1,20 @@
+from monkeystuff import say_hello
+from io import StringIO
+import os
+importt pytest
+
+
+# def test_something(monkeypatch):
+#     # replace the os.path.join function with a lambda!
+#     monkeypatch.setattr('os.path.join', lambda x: '/etc/passwd')
+#     print(os.path.join('stuff'))
+
+@pytest.fixture
+def fake_name_input():
+    s = StringIO('Reuven\n')
+
+
+def test_say_hello(monkeypatch, fake_name_input):
+    monkeypatch.setat
+    output = say_hello()
+    assert output == 'Hello, Reuven!'
