9dc24b5dc1970cf7ffea0d821bfe33af3abdfa07
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 21:53:32 +0300

    gitautosync update 326

--- flycheck_test_phone.py
+++ flycheck_test_phone.py
@@ -1,6 +1,7 @@
 import pytest
 from phone import Phone
 
+
 @pytest.fixture
 def sample_phone():
     return Phone(1)
@@ -17,11 +18,10 @@ def test_id_number(sample_phone):
 def test_make_some_calls(sample_phone):
     sample_phone.call('12345')
     sample_phone.call('67890')
-    assert '12345' in p.call_history
-    assert '67890' in p.call_history
+    assert '12345' in sample_phone.call_history
+    assert '67890' in sample_phone.call_history
 
 
-def test_bad_phone_number():
-    p = Phone(1)
+def test_bad_phone_number(sample_phone):
     with pytest.raises(ValueError):
-        p.call('badnum1')
+        sample_phone.call('badnum1')
