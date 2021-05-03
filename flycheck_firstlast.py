5d6efcfab799a8bea910104722b242c0ccd4926d
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 19:51:01 +0300

    gitautosync update 245

---
+++ flycheck_firstlast.py
@@ -0,0 +1,17 @@
+def firstlast(seq):
+    if not seq:
+        return seq
+
+    
+
+    if isinstance(seq, str):
+        return seq[0] + seq[-1]
+
+    elif isinstance(seq, list):
+        return [seq[0], seq[-1]]
+
+    elif isinstance(seq, tuple):
+        return (seq[0], seq[-1])
+
+    else:
+        raise TypeError('Not a sequence!')
