24e0d39fb8ea93969759e6814679719ce84c4ea2
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 21:53:21 +0300

    gitautosync update 325

--- test_phone.py
+++ test_phone.py
@@ -2,25 +2,26 @@ import pytest
 from phone import Phone
 
 
-def test_empty_phone():
-    p = Phone(1)
-    assert p.call_history == []
+@pytest.fixture
+def sample_phone():
+    return Phone(1)
 
 
-def test_id_number():
-    p = Phone(1)
-    assert p.id_number == 1
+def test_empty_phone(sample_phone):
+    assert sample_phone.call_history == []
 
 
-def test_make_some_calls():
-    p = Phone(1)
-    p.call('12345')
-    p.call('67890')
-    assert '12345' in p.call_history
-    assert '67890' in p.call_history
+def test_id_number(sample_phone):
+    assert sample_phone.id_number == 1
 
 
-def test_bad_phone_number():
-    p = Phone(1)
+def test_make_some_calls(sample_phone):
+    sample_phone.call('12345')
+    sample_phone.call('67890')
+    assert '12345' in sample_phone.call_history
+    assert '67890' in sample_phone.call_history
+
+
+def test_bad_phone_number(sample_phone):
     with pytest.raises(ValueError):
-        p.call('badnum1')
+        sample_phone.call('badnum1')
