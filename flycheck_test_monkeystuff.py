5680b85d831a4ad616f1673a77fb91413c0c4db2
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 22:56:29 +0300

    gitautosync update 20

--- flycheck_test_monkeystuff.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def test_something(monkeypatch):
-    monkeypatch.setattr('os.pathsep', '||')
-    print(os.pathsep)
-    print(os.path.join('abcd', 'efgh', 'ijkl'))
--- test_monkeystuff.py
+++ test_monkeystuff.py
@@ -3,4 +3,5 @@ import os
 
 def test_something(monkeypatch):
     monkeypatch.setattr('os.pathsep', '||')
-    print(os.path.join('abcd', 'efgh', 'ijkl'))
+    print(os.pathsep)
+    # print(os.path.join('abcd', 'efgh', 'ijkl'))
