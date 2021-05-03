32cdf0b541ad27f0f8c45b8f757f740c6ec7ed0d
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 22:58:58 +0300

    gitautosync update 27

--- test_monkeystuff.py
+++ test_monkeystuff.py
@@ -2,5 +2,6 @@ import os
 
 
 def test_something(monkeypatch):
+    # replace the os.path.join function with a lambda!
     monkeypatch.setattr('os.path.join', lambda x: '/etc/passwd')
     print(os.path.join('stuff'))
