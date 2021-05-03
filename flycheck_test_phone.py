f0f5f1be3dc0f0d828db694dffab5095a6dbb2e2
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 22:27:01 +0300

    gitautosync update 365

--- flycheck_test_phone.py
+++ flycheck_test_phone.py
@@ -19,7 +19,9 @@ def test_id_number(sample_phone):
     assert sample_phone.id_number == 1
 
 
-def test_make_some_calls(sample_phone):
+def test_make_some_calls(sample_phone, good_phone_numbers):
+    for one_phone_number in good_phone_numbers:
+        
     sample_phone.call('12345')
     sample_phone.call('67890')
     assert '12345' in sample_phone.call_history
