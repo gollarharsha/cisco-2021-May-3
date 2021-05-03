869d19a441b7f06a036c033c43b40ce2e792912a
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 19:47:48 +0300

    gitautosync update 231

--- flycheck_test_firstlast.py
+++ flycheck_test_firstlast.py
@@ -4,6 +4,6 @@ import pytest
 @pytest.mark.parametrize('seq,result',
                          [('abcde', 'ae'),
                           ([10, 20, 30, 40, 50, 60], [10, 60]),
-                          ((100, 200), (100, 200)
-
-def test_firstlast()
+                          ((100, 200), (100, 200))])
+def test_firstlast(seq, result):
+    
