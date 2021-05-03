4af83e20700f2c699b1ca4358bd21eca3857f2d2
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 18:02:13 +0300

    gitautosync update 105

---
+++ flycheck_test_myadd.py
@@ -0,0 +1,18 @@
+from myadd import add
+
+
+def test_add_positive_ints():
+
+    assert add(2, 2) == 4
+
+
+def test_add_negative_ints():
+    assert add(-2, -2) == -4
+
+
+def test_add_mixed_ints():
+    assert add(2, -2) == 0
+
+
+def test_add_strings():
+    assert add('abcd', 'efgh') == 'abcdefgh'
