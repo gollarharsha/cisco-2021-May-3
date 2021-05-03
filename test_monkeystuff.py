81c0f2e3c93374dc053e1e179155b682a15c60dc
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 23:01:38 +0300

    gitautosync update 34

--- test_monkeystuff.py
+++ test_monkeystuff.py
@@ -1,11 +1,18 @@
 from monkeystuff import say_hello
+from io import StringIO
 import os
+importt pytest
 
 
 # def test_something(monkeypatch):
 #     # replace the os.path.join function with a lambda!
 #     monkeypatch.setattr('os.path.join', lambda x: '/etc/passwd')
 #     print(os.path.join('stuff'))
+
+@pytest.fixture
+def fake_name_input():
+    s = StringIO('Reuven\n')
+
 
 def test_say_hello():
     output = say_hello()
