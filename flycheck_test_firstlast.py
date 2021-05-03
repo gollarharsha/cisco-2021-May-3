1b3073e698c43626ce99eafadd1e795039c810cb
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 19:49:10 +0300

    gitautosync update 237

---
+++ flycheck_test_firstlast.py
@@ -0,0 +1,18 @@
+from firstlast import firstlast
+import pytest
+
+
+@pytest.mark.parametrize('seq,result',
+                         [('', ''),
+                          ([], []),
+                          ((), ()),
+                          ('abcde', 'ae'),
+                          ([10, 20, 30, 40, 50, 60], [10, 60]),
+                          ((100, 200), (100, 200))])
+def test_firstlast(seq, result):
+    assert firstlast(seq) == result
+
+@pytest.mark.parametrize(
+def test_nonsequence():
+    with pytest.raises(TypeError):
+        firstlast(
