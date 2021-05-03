c6bbec5344b4630fba81459309a8637cc252e5f8
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 23:14:11 +0300

    gitautosync update 52

--- flycheck_test_add_numbers.py
+++ flycheck_test_add_numbers.py
@@ -2,11 +2,13 @@ from add_numbers import add_numbers
 from io import StringIO
 import pytest
 
+
 @pytest.fixture
 def fake_input():
-    return StringIO('10 20')
+    return StringIO('10 20\n')  
+
 
 def test_simple_adding(monkeypatch, fake_input):
     monkeypatch.setattr('sys.stdin', fake_input)
     value = add_numbers()
-    assert va
+    assert value == 30
--- test_add_numbers.py
+++ test_add_numbers.py
@@ -2,10 +2,13 @@ from add_numbers import add_numbers
 from io import StringIO
 import pytest
 
+
 @pytest.fixture
 def fake_input():
-    return StringIO('10 20')
+    return StringIO('10 20\n')
+
 
 def test_simple_adding(monkeypatch, fake_input):
     monkeypatch.setattr('sys.stdin', fake_input)
-    retu
+    value = add_numbers()
+    assert value == 30
