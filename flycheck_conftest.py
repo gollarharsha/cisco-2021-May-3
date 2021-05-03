9449e57da54cf1217809d7cefd9256a0911b68e1
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 22:48:40 +0300

    gitautosync update 378

---
+++ flycheck_conftest.py
@@ -0,0 +1,19 @@
+from phone import Phone
+from person import Person
+import pytest
+
+
+@pytest.fixture(scope='session')
+def a_person():
+    p = Person('first1', 'last1')
+    return p
+
+
+@pytest.fixture
+def sample_phone(au):
+    return Phone(1)
+
+
+@pytest.fixture
+def good_phone_numbers():
+    return ['12345', '67890', '2468', '1357']
