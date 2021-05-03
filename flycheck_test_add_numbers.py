b45572e5690ef749f1c86b115f97dfb4d807879b
Reuven Lerner <reuven@lerner.co.il>
2021-05-03 23:13:16 +0300

    gitautosync update 48

--- flycheck_test_add_numbers.py
+++ flycheck_test_add_numbers.py
@@ -1,7 +1,9 @@
 from add_numbers import add_numbers
 from io import StringIO
+import pytest
 
+@pytest.fixture
 def fake_input():
-    s = StringIO
+    s = StringIO('10 20
 
 def test_simple_adding(monkeypatch, 
